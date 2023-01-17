4e39466383613d57104473dc4cf85f7f6a8c6ca6
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2023-01-17 14:28:27 -0500

    added more code

--- js/script.js
+++ js/script.js
@@ -16,6 +16,17 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
+
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  var numberB = parseFloat(document.getElementById("denomenator").value)
+  var numberA = parseFloat(document.getElementById("numerator").value)
+  var counter = 0
+  var remainder = numberA
+
+  while (remainder >= numberB) {
+    remainder = remainder - numberB
+    counter++
+  }
+  document.getElementById("quotient").innerHTML =
+    "You have a quotient of  " + counter + " R " + remainder + "."
 }
